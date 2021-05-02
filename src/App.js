959d40eda6c646bfea2cda5e68fc3499b475169c
amnkhan <amnkhan.me@gmail.com>
2021-05-02 22:44:15 +0600

    Added react-markdown to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
 export default function App() {
+  const [userInput, setUserInput] = useState(`# Lets type some markdown!`);
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
   return (
     <div className="app">
-      <textarea />
-
-      <div className="preview" />
+      <textarea value={userInput} onChange={handleChange} />
+      <ReactMarkdown className="preview" children={userInput} />
     </div>
   );
 }
